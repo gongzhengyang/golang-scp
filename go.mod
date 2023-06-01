bbc6621ca4fea9dbcff2ea6cf34be90c9c125448
gongzhengyang <gongzhengyang@bolean.com.cn>
2023-06-01 15:24:38 +0800

    feat: Init and finished

---
+++ go.mod
@@ -0,0 +1,10 @@
+module golang-scp
+
+go 1.20
+
+require (
+	github.com/bramvdbogaerde/go-scp v1.2.1
+	golang.org/x/crypto v0.9.0
+)
+
+require golang.org/x/sys v0.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	scp "github.com/bramvdbogaerde/go-scp"
+	"github.com/bramvdbogaerde/go-scp/auth"
+	"golang.org/x/crypto/ssh"
+	"os"
+)
+
+type Config struct {
+	Username  string
+	Password  string
+	Ipaddress string
+	Port      uint16
+	Filename  string
+}
+
+// a tool for scp file into remote server
+// $ CONFIG='{"ipaddress": "10.30.6.101", "filename": "some-file", "username": "my-user", "password": "my-pass", "port": 22}' ./golang-scp
+// this will copy `some-file` to remote server
+func main() {
+	configStr := os.Getenv("CONFIG")
+	var config Config
+
+	err := json.Unmarshal([]byte(configStr), &config)
+	if err != nil {
+		fmt.Println("unmarshal failed with ", err)
+		return
+	}
+	clientConfig, _ := auth.PasswordKey(config.Username, config.Password, ssh.InsecureIgnoreHostKey())
+	remote := fmt.Sprintf("%s:%d", config.Ipaddress, config.Port)
+	client := scp.NewClient(remote, &clientConfig)
+	err = client.Connect()
+	if err != nil {
+		fmt.Println("Couldn't establish a connection to the remote server ", err)
+		return
+	}
+
+	f, _ := os.Open(config.Filename)
+
+	defer client.Close()
+	defer f.Close()
+	//remoteFilepath := fmt.Sprintf("~/%s", config.Filename)
+	//fmt.Println("remote filepath ", remoteFilepath)
+	err = client.CopyFromFile(context.Background(), *f, config.Filename, "0777")
+	if err != nil {
+		fmt.Println("Error while copying file ", err)
+	}
+}
